0d4b1b339c02cc9d6c8b0f8c37520a46609933f7
cjen07 <wangcjen@gmail.com>
2020-02-09 13:17:38 +0800

    two problems

--- 1_two-sum.cpp
+++ 1_two-sum.cpp
@@ -1,13 +1,14 @@
+// https://leetcode-cn.com/problems/two-sum/
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int *p; 
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *p;
     p = (int*)malloc(2*sizeof(int));
-    for(int a=0;a<numsSize;a++){
-        for(int b=0;b<numsSize;b++){
-            if(a!=b){
-                if (nums[a]+nums[b]==target){
+    for(int a=0; a<numsSize; a++) {
+        for(int b=0; b<numsSize; b++) {
+            if(a!=b) {
+                if (nums[a]+nums[b]==target) {
                     p[0]=a;
                     p[1]=b;
                     *returnSize=2;
@@ -21,8 +22,8 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
 vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> result;
-    for(int i = 0; i < nums.size() - 1; i++){
-        for(int j = i + 1; j < nums.size(); j++){
+    for(int i = 0; i < nums.size() - 1; i++) {
+        for(int j = i + 1; j < nums.size(); j++) {
             if (nums[i]+nums[j]==target) {
                 result.push_back(i);
                 result.push_back(j);
@@ -39,10 +40,10 @@ public:
         map<int, int> a;
         map<int,int>::iterator it;
         vector<int> result;
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i < nums.size(); i++) {
             int diff = target - nums[i];
             it = a.find(diff);
-            if (it != a.end()){
+            if (it != a.end()) {
                 result.push_back(it->second);
                 result.push_back(i);
                 return result;
--- 7_reverse-integer.cpp
+++ 7_reverse-integer.cpp
@@ -20,34 +20,35 @@
 // x < 0
 // n < 0
 // 判断n = n * 10 + a; n是否溢出？
-// 如果 n = (-m - a) / 10 - 1 
+// 如果 n = (-m - a) / 10 - 1
 // n * 10 + a = -m + r - 10 (其中r为-m-a的个位数字, r是正数)
 // 如果肯定溢出
 // 如果 n = (-m - a) / 10是否溢出？
 // n * 10 + a = -m + r (其中r为-m-a的个位数字, r是正数) 肯定不溢出
 // 所以 当 n >= (-m - a) / 10 时 不溢出 否则会溢出
 
-int reverse(int x){
-    long long n=0,m=1,a;
-    for (int i=0;i<31;i++)
+int reverse(int x) {
+    int n=0,m=1,a;
+    for (int i=0; i<30; i++)
         m=m*2;
+    int int_max = (m - 1) * 2 + 1;
+    int int_min = (-m) * 2;
     if (x > 0) {
-        while (x!=0){
+        while (x!=0) {
             a=x%10;
             x=x/10;
-            if (n > (m - 1 - a) / 10) {
+            if (n > (int_max - a) / 10)
                 return 0;
-            }
             n=n*10+a;
         }
         return n;
     }
     else {
-        while (x!=0){
+        while (x!=0) {
             a=x%10;
             x=x/10;
 
-            if (n < (-m - a) / 10)
+            if (n < (int_min - a) / 10)
                 return 0;
 
             n=n*10+a;
